58a89f8ecabf23742baec03520c6248746c4696b
nanditha <nandithahari6@gmail.com>
2023-11-21 22:12:01 +0530

    Final

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import SignupForm from "./SignupForm";
 import React ,{useState}from "react";
 import SignupFormSuccess from "./SignupFotmSuccess";
 import CustomerPage from "../customer/CustomerPage";
-import BankPage from "../bank/App";
+import BankPage from "../bank/App.jsx";
 const Form=()=>{
     const[formIsSubmitted,setFormIsSubmitted]=useState(false);
   const [user,setUser] = useState("");
