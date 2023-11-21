64280ddfdcfe9f65aa907cc77e7f76339538554b
nanditha <nandithahari6@gmail.com>
2023-11-21 22:21:31 +0530

    bank2

--- src/components/SignupFotmSuccess.jsx
+++ src/components/SignupFotmSuccess.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import "./success.css";
 import useForm from "./useForm";
-import CustomerPage from "../customer/CustomerPage";
-import BankPage from "../bank/BankPage";
+
 const SignupFormSuccess=({submitForm})=>{
     const { handleChange, handleRegFormSubmit, values, errors } = useForm(submitForm);
     return(
         <div className="container1"  >
             <div >
-                
-            {console.log(values.userType==="customer")}
-            {values.userType === 'customer' ?  (<CustomerPage />):(<BankPage/>)}
+             
                  
             </div>
         </div>
